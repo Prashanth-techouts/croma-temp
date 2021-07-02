1c7403fb97f03bc1f067de979a6f8f8cbdb7021f
SaiBhavaniPuli <saibhavani.p@techouts.com>
2021-07-02 22:25:39 +0530

    first commit

---
+++ pocchroma/pocchromacockpits/gensrc/de/techouts/pocchroma/cockpits/constants/GeneratedPocchromaCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 2 Jul, 2021 8:55:01 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package de.techouts.pocchroma.cockpits.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedPocchromaCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "pocchromacockpits";
+	
+	protected GeneratedPocchromaCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ pocchroma/pocchromainitialdata/gensrc/de/techouts/pocchroma/initialdata/constants/GeneratedPocchromaInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 2 Jul, 2021 8:55:01 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package de.techouts.pocchroma.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedPocchromaInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "pocchromainitialdata";
+	
+	protected GeneratedPocchromaInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
